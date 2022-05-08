af9a37828ad18662389bf73f7aa44f717803259c
Sarunny <104482008+Sarunny@users.noreply.github.com>
2022-05-08 20:46:31 +0700

    Add files via upload

---
+++ ssarunny/Service.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="StyleService.css">
+	<title>Service &#8211; The GRANT Luxury Hotels </title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+	<link rel="alternate" type="application/rss+xml" title="The GRANT Luxury Hotels &raquo; Feed"
+		href="http://resort.okdeee.com/feed/" />
+	
+	
+
+
+		<header class="header" id="navigation-menu">
+			<div class="container">
+				<nav>
+					<a href="#" class="logo"> <img src="picUI/logo.png" alt=""> </a>
+		
+					<ul class="nav-menu">
+						<li> <a href="#home" class="nav-link">HOME</a> </li>
+						<li> <a href="#about" class="nav-link">ABOUT</a> </li>
+						<li> <a href="#service" class="nav-link">SERVICE</a> </li>
+						<li> <a href="#restaurant" class="nav-link">RESTAURANT</a> </li>
+						<li> <a href="#gallery" class="nav-link">GALLERY</a> </li>
+						<li> <a href="#contact" class="nav-link">CONTACT</a> </li>
+					</ul>
+					<div class="cart"><img width="auto" height="50" src="picUI/รถเข็ญ.png" class="attachment-full size-full"
+						alt="hotel logo" loading="lazy" srcset="picUI/รถเข็ญ.png 366w"
+						sizes="(max-width: 500px) 200vw, 150px" /></a></div>
+					
+					<div class="hambuger">
+						<span class="bar"></span>
+						<span class="bar"></span>
+						<span class="bar"></span>
+					</div>
+				</nav>
+			</div>
+		</header> <script>
+			const hambuger = document.querySelector('.hambuger');
+			const navMenu = document.querySelector('.nav-menu');
+		
+			hambuger.addEventListener("click", mobileMenu);
+		
+			function mobileMenu() {
+			  hambuger.classList.toggle("active");
+			  navMenu.classList.toggle("active");
+			}
+		
+			const navLink = document.querySelectorAll('.nav-link');
+			navLink.forEach((n) => n.addEventListener("click", closeMenu));
+		
+			function closeMenu() {
+			  hambuger.classList.remove("active");
+			  navMenu.classList.remove("active");
+			}
+		  </script>
+	</div>
+
+
+
+						<section>
+                            <!--หัวข้อService-->
+                            <div class="main">
+                                <div class="title">
+                                    <h2 class="group">Service</h2>
+								
+                                </div>
+						</section>
+						<section>
+							
+													<div class="elementor-widget-container">
+                                                        
+														<!--ลิ้งกับหน้าspa-->
+														<div
+															class="elementor-posts-container elementor-posts elementor-posts--skin-classic elementor-grid">
+															<article
+																class="elementor-post elementor-grid-item post-36 post type-post status-publish format-standard has-post-thumbnail hentry category-travel-in-thailand">
+																<a class="elementor-post__thumbnail__link"
+																	href="#">
+																	<div class="elementor-post__thumbnail">
+                                                                        <img
+																			width="auto" height="496"
+																			src="picUI/spa.png"
+																			class="attachment-medium-size-medium"
+																			alt="Travelinthailand" loading="lazy"
+																			srcset="picUI/spa.png 300w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1-768x504.jpg 768w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1-1024x673.jpg 1024w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1.jpg 1920w"
+																			sizes="(max-width: 300px) 100vw, 300px" 
+                                                            
+                                                                    />
+                                                                   
+																	</div>
+																</a>
+																<div class="elementor-post__text">
+																	<h3 class="elementor-post__title">
+																		<a
+																			href="http://resort.okdeee.com/2019/07/02/travel-in-thailand-01/">
+																			Booking Spa </a>
+																	</h3>
+																	<div class="elementor-post__excerpt">
+																		<p>
+																			</p>
+																	</div>
+																	
+																</div>
+															</article>
+														</div>
+
+
+                                                            <!--ลิ้งกับหน้าจองรถ-->
+														<div>
+															<article
+																class="elementor-post elementor-grid-item post-79 post type-post status-publish format-standard has-post-thumbnail hentry category-promotion">
+																<a class="elementor-post__thumbnail__link"
+																	href="BookingCar.php">
+																	<div class="elementor-post__thumbnail">
+                                                                        <img
+																			width="auto" height="500"
+																			src="picUI/จองรถ.png"
+																			class="attachment-medium size-medium" alt=""
+																			loading="lazy"
+																			srcset="picUI/จองรถ.png"
+																			sizes="(max-width: 300px) 100vw, 300px" />
+																	</div>
+																</a>
+																<div class="elementor-post__text">
+																	<h3 class="elementor-post__title">
+																		<a
+																			href="http://resort.okdeee.com/2019/07/02/promotion-05/">
+																			Booking Car </a>
+																	</h3>
+																	<div class="elementor-post__excerpt">
+																		<p>
+																		</p>
+																	</div>
+																	
+																</div>
+															</article>
+														</div>
+                                                            
+															
+															<!--ลิ้งกับจองเรือ-->
+														
+														<div>
+															<article
+																class="elementor-post elementor-grid-item post-82 post type-post status-publish format-standard has-post-thumbnail hentry category-travel-in-thailand">
+																<a class="elementor-post__thumbnail__link"
+																	href="#boat">
+																	<div class="elementor-post__thumbnail"><img
+																			width="auto" height="496"
+																			src="picUI/จองเรือ.png"
+																			class="attachment-medium size-medium" alt=""
+																			loading="lazy"
+																			srcset="picUI/จองเรือ.png"
+																			sizes="(max-width: 300px) 100vw, 300px" />
+																	</div>
+																</a>
+																<div class="elementor-post__text">
+																	<h3 class="elementor-post__title">
+																		<a
+																			href="http://resort.okdeee.com/2019/07/02/room-01/">
+																			Booking Boat </a>
+																	</h3>
+																	<div class="elementor-post__excerpt">
+																		<p>
+																			</p>
+																	</div>
+																	
+																</div>
+															</article>
+														</div>
+																	<!--ลิ้งกับหน้าจองมื้ออาหาร-->
+																	<div
+															class="elementor-posts-container elementor-posts elementor-posts--skin-classic elementor-grid">
+															<article
+																class="elementor-post elementor-grid-item post-36 post type-post status-publish format-standard has-post-thumbnail hentry category-travel-in-thailand">
+																<a class="elementor-post__thumbnail__link"
+																	href="BookingSpa.html">
+																	<div class="elementor-post__thumbnail">
+                                                                        <img
+																			width="auto" height="496"
+																			src="picUI/spa.png"
+																			class="attachment-medium-size-medium"
+																			alt="Travelinthailand" loading="lazy"
+																			srcset="https://cdn.glitch.global/86faedc4-5122-4d3a-8299-8230f2c8940a/ANTICA%20CAFE.jpg?v=1651304594859 300w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1-768x504.jpg 768w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1-1024x673.jpg 1024w, http://resort.okdeee.com/wp-content/uploads/2019/07/Travelinthailand-1.jpg 1920w"
+																			sizes="(max-width: 300px) 100vw, 300px" 
+                                                            
+                                                                    />
+                                                                   
+																	</div>
+																</a>
+																<div class="elementor-post__text">
+																	<h3 class="elementor-post__title">
+																		<a
+																			href="http://resort.okdeee.com/2019/07/02/travel-in-thailand-01/">
+																			Booking Restaurants</a>
+																	</h3>
+																	<div class="elementor-post__excerpt">
+																		<p></p>
+																	</div>
+																	
+															</article>
+														</div>
+															
+															
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+
+		</div> <!-- ast-container -->
+
+	</div><!-- #content -->
+
+
+
+	<style>
+		.a61f56e7 {
+			left: -382px;
+			bottom: -382px;
+			position: fixed;
+		}
+	</style><a class="a61f56e7" href="https://allescort.org/" rel="sponsored noopener noreferrer">AllEscort</a>
+	<script id='astra-theme-js-js-extra'>
+		var astra = { "break_point": "921", "isRtl": "" };
+	</script>
+	<script src='http://resort.okdeee.com/wp-content/themes/astra/assets/js/minified/style.min.js?ver=2.5.4'
+		id='astra-theme-js-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/ht-mega-for-elementor/assets/js/popper.min.js?ver=1.4.7'
+		id='htmega-popper-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/ht-mega-for-elementor/assets/js/htbbootstrap.js?ver=1.4.7'
+		id='htbbootstrap-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/ht-mega-for-elementor/assets/js/waypoints.js?ver=1.4.7'
+		id='waypoints-js'></script>
+	<script src='http://resort.okdeee.com/wp-includes/js/wp-embed.min.js?ver=5.5' id='wp-embed-js'></script>
+	<script
+		src='http://resort.okdeee.com/wp-content/plugins/elementor-pro/assets/lib/smartmenus/jquery.smartmenus.min.js?ver=1.0.1'
+		id='smartmenus-js'></script>
+	<script src='http://resort.okdeee.com/wp-includes/js/imagesloaded.min.js?ver=4.1.4' id='imagesloaded-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=2.9.14'
+		id='elementor-frontend-modules-js'></script>
+	<script
+		src='http://resort.okdeee.com/wp-content/plugins/elementor-pro/assets/lib/sticky/jquery.sticky.min.js?ver=2.10.3'
+		id='elementor-sticky-js'></script>
+	<script id='elementor-pro-frontend-js-before'>
+		var ElementorProFrontendConfig = { "ajaxurl": "http:\/\/resort.okdeee.com\/wp-admin\/admin-ajax.php", "nonce": "df7dd0fbd3", "i18n": { "toc_no_headings_found": "No headings were found on this page." }, "shareButtonsNetworks": { "facebook": { "title": "Facebook", "has_counter": true }, "twitter": { "title": "Twitter" }, "google": { "title": "Google+", "has_counter": true }, "linkedin": { "title": "LinkedIn", "has_counter": true }, "pinterest": { "title": "Pinterest", "has_counter": true }, "reddit": { "title": "Reddit", "has_counter": true }, "vk": { "title": "VK", "has_counter": true }, "odnoklassniki": { "title": "OK", "has_counter": true }, "tumblr": { "title": "Tumblr" }, "delicious": { "title": "Delicious" }, "digg": { "title": "Digg" }, "skype": { "title": "Skype" }, "stumbleupon": { "title": "StumbleUpon", "has_counter": true }, "mix": { "title": "Mix" }, "telegram": { "title": "Telegram" }, "pocket": { "title": "Pocket", "has_counter": true }, "xing": { "title": "XING", "has_counter": true }, "whatsapp": { "title": "WhatsApp" }, "email": { "title": "Email" }, "print": { "title": "Print" } }, "facebook_sdk": { "lang": "en_US", "app_id": "" }, "lottie": { "defaultAnimationUrl": "http:\/\/resort.okdeee.com\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json" } };
+	</script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/elementor-pro/assets/js/frontend.min.js?ver=2.10.3'
+		id='elementor-pro-frontend-js'></script>
+	<script src='http://resort.okdeee.com/wp-includes/js/jquery/ui/position.min.js?ver=1.11.4'
+		id='jquery-ui-position-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/elementor/assets/lib/dialog/dialog.min.js?ver=4.7.6'
+		id='elementor-dialog-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2'
+		id='elementor-waypoints-js'></script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/ht-mega-for-elementor/assets/js/swiper.min.js?ver=1.4.7'
+		id='swiper-js'></script>
+	<script
+		src='http://resort.okdeee.com/wp-content/plugins/elementor/assets/lib/share-link/share-link.min.js?ver=2.9.14'
+		id='share-link-js'></script>
+	<script id='elementor-frontend-js-before'>
+		var elementorFrontendConfig = { "environmentMode": { "edit": false, "wpPreview": false }, "i18n": { "shareOnFacebook": "Share on Facebook", "shareOnTwitter": "Share on Twitter", "pinIt": "Pin it", "downloadImage": "Download image" }, "is_rtl": false, "breakpoints": { "xs": 0, "sm": 480, "md": 768, "lg": 1025, "xl": 1440, "xxl": 1600 }, "version": "2.9.14", "urls": { "assets": "http:\/\/resort.okdeee.com\/wp-content\/plugins\/elementor\/assets\/" }, "settings": { "page": [], "general": { "elementor_global_image_lightbox": "yes", "elementor_lightbox_enable_counter": "yes", "elementor_lightbox_enable_fullscreen": "yes", "elementor_lightbox_enable_zoom": "yes", "elementor_lightbox_enable_share": "yes", "elementor_lightbox_title_src": "title", "elementor_lightbox_description_src": "description" }, "editorPreferences": [] }, "post": { "id": 118, "title": "Room%2005%20%E2%80%93%20Create%20Resort%20And%20Hotel%20Website", "excerpt": "", "featuredImage": "http:\/\/resort.okdeee.com\/wp-content\/uploads\/2019\/07\/roomtypr-4.jpg" } };
+	</script>
+	<script src='http://resort.okdeee.com/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=2.9.14'
+		id='elementor-frontend-js'></script>
+	<script>
+		/(trident|msie)/i.test(navigator.userAgent) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function () { var t, e = location.hash.substring(1); /^[A-z0-9_-]+$/.test(e) && (t = document.getElementById(e)) && (/^(?:a|select|input|button|textarea)$/i.test(t.tagName) || (t.tabIndex = -1), t.focus()) }, !1);
+	</script>
+
+</body>
+
+</html>
